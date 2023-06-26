f2b49116c29b0babf2c7fb39a7540d5b07e09db2
buchonsi <buchonsi@naver.com>
2023-06-27 01:01:33 +0900

    [카카오 주소 검색하기 api] - 통합테스트 환경 구축
    * testContainer을 사용하여 테스트
    * testContainer를 위한 mariaDb, redis 설정 추가

---
+++ src/main/java/com/example/recophar/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,8 @@
+package com.example.recophar.pharmacy.repository;
+
+import com.example.recophar.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
