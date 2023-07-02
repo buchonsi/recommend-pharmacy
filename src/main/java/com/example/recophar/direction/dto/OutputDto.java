85760d6580675a11cd48fb5975d92436fd8dafa3
buchonsi <buchonsi@naver.com>
2023-07-03 01:10:50 +0900

    주소검색 컨트롤러 및 응답 페이지 작성

---
+++ src/main/java/com/example/recophar/direction/dto/OutputDto.java
@@ -0,0 +1,14 @@
+package com.example.recophar.direction.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class OutputDto {
+    private String pharmacyName;
+    private String pharmacyAddress;
+    private String directionUrl;
+    private String roadViewUrl;
+    private String distance;
+}
--- src/main/java/com/example/recophar/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/recophar/pharmacy/service/PharmacyRecommendationService.java
@@ -3,15 +3,19 @@ package com.example.recophar.pharmacy.service;
 import com.example.recophar.api.dto.DocumentDto;
 import com.example.recophar.api.dto.KakaoApiResponseDto;
 import com.example.recophar.api.service.KakaoAddressSearchService;
+import com.example.recophar.direction.dto.OutputDto;
 import com.example.recophar.direction.entity.Direction;
 import com.example.recophar.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,19 +25,50 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentDtoList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentDtoList().get(0);
 
-//        List<Direction> directionList = directionService.buildDirectionList(documentDto);
-        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
-        directionService.saveAll(directionList);
+        //공공기관 약국 데이터 및 거리계산 알고리즘 이용
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        //kakao 카테고리를 이용한 장소 검색 api 이용
+        //List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        return directionService.saveAll(directionList).stream()
+                .map(direction -> convertToOutputDto(direction))
+                .collect(Collectors.toList());
+    }
+
+    private OutputDto convertToOutputDto(Direction direction) {
+
+        String params = String.join(",",
+                direction.getTargetPharmacyName(),      //한글은 인코딩 필요
+                String.valueOf(direction.getTargetLatitude()),
+                String.valueOf(direction.getTargetLongitude())
+        );
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
+                .toUriString();
+
+        log.info("direction params: {} , url: {}", params, result);
+
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
