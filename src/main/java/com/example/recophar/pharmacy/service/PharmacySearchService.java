4245d868ee54874d0319a100e4b942fcbf25ef9e
buchonsi <buchonsi@naver.com>
2023-07-05 01:45:44 +0900

    레디스로 성능개선2 - 레디스 캐시 생성 및 접근 로직

--- src/main/java/com/example/recophar/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/recophar/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.example.recophar.pharmacy.service;
 
+import com.example.recophar.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.recophar.pharmacy.dto.PharmacyDto;
 import com.example.recophar.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,15 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
         //redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(!pharmacyDtoList.isEmpty()) {
+            log.info("[PharmacySearchService searchPharmacyDtoList] redis findAll success!");
+            return pharmacyDtoList;
+        }
 
         //db
         return pharmacyRepositoryService.findAll()
